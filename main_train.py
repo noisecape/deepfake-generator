d7eb21e9c70fc82a7dc881e433f579e04c104405
Noisecape <tommycaps@hotmail.it>
2024-02-28 22:20:31 +0000

    refactored main train script

--- main_train.py
+++ main_train.py
@@ -4,14 +4,28 @@ from torch.utils.data import DataLoader
 from albumentations.pytorch.transforms import ToTensorV2
 import albumentations as A
 
+N_LANDMARKS = 468
+
 if __name__ == "__main__":
 
-    df = pd.read_pickle(r"C:\Users\Noisecape\AI\Datasets\dataset.pkl")
+    df = pd.read_pickle(r"C:\Users\Noisecape\AI\Datasets\dataset_processed.pkl")
+
     transforms = A.Compose({
         A.Resize(224, 224),
         ToTensorV2()
         })
-    dataset = MaskFace(df, perc=0.45, transforms=transforms)
+
+    min_triangles = df['#_triangles'].min()
+
+    n_triangles_predictor = int(min_triangles * 0.55)
+    n_triangles_encoder = int(min_triangles * 0.75)
+    
+    dataset = MaskFace(
+        df,
+        n_triangles_predictor=n_triangles_predictor, 
+        n_triangles_encoder=n_triangles_encoder, 
+        transforms=transforms
+        )
 
     dataloader = DataLoader(dataset, batch_size=3, shuffle=True)
 
